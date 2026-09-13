8d9a509007de9cb00e124f78949d301c44e750b8
blitzzc <bekzatmazikenov@gmail.com>
2026-09-13 16:49:32 +0500

    builder

---
+++ src/SpaceshipBuilder.java
@@ -0,0 +1,48 @@
+public class SpaceshipBuilder {
+    private static final int MIN_SHIELDSTRENGTH = 20;
+    private static final String DEFAULT_WEAPONS = "None";
+
+    private String hull;
+    private String engine;
+    private int shieldStrength;
+    private String weapons = DEFAULT_WEAPONS;
+    private boolean hasLifeSupport;
+
+    public SpaceshipBuilder setHull(String hull) {
+        this.hull = hull;
+        return this;
+    }
+
+    public SpaceshipBuilder setEngine(String engine) {
+        this.engine = engine;
+        return this;
+    }
+
+    public SpaceshipBuilder setShieldStrength(int shieldStrength) {
+        this.shieldStrength = shieldStrength;
+        return this;
+    }
+
+    public SpaceshipBuilder setWeapons(String weapons) {
+        this.weapons = weapons;
+        return this;
+    }
+
+    public SpaceshipBuilder setHasLifeSupport(boolean hasLifeSupport) {
+        this.hasLifeSupport = hasLifeSupport;
+        return this;
+    }
+
+    public Spaceship build() {
+        if (hull == null || hull.isEmpty()) {
+            throw new IllegalStateException("Cannot build a Spaceship without hull");
+        }
+        if (engine == null || engine.isEmpty()) {
+            throw new IllegalStateException("Cannot build a Spaceship without engine");
+        }
+        if (shieldStrength < MIN_SHIELDSTRENGTH) {
+            throw new IllegalStateException("Shield Strength must be at least " + MIN_SHIELDSTRENGTH);
+        }
+        return new Spaceship(hull, engine, shieldStrength, weapons, hasLifeSupport);
+    }
+}
